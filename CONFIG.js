8c37f1067ef32f75ca3cc61ba9f2638fe7c0aef0
tranthanhtungbg <144113807+tranthanhtungbg@users.noreply.github.com>
2025-03-07 14:23:19 +0700

    Add files via upload

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "💐 Chúc mừng 8/3! 💐 </br> Chúc cậu một ngày tràn đầy niềm vui, luôn xinh đẹp rạng ngời nhé 🌸💖",
+  descriptionHeader:
+    "Hôm nay là ngày đặc biệt, tớ đã có một kế hoạch nho nhỏ dành riêng cho cậu rồi! Đi chơi với tớ nhé 🎉✨",
+  buttonYes: "Tớ đồng ý",
+  buttonNo: "Không đồng ý",
+  titleModar: "Cảm ơn cậu.",
+  descriptionModar:
+    "Vậy hẹn cậu 7h tối nay nhé. Cậu không cần mang mũ đâu, tớ đi ô tô.",
+};
