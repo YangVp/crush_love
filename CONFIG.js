3b37f152d17265e22b428266573fb8b411fd14a8
YangVp <saomai492000@gmail.com>
2025-06-13 13:06:45 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,5 +1,5 @@
 const CONFIGDATA = {
-  titleHeader: "💐 Chúc mừng 8/3! 💐 </br> Chúc em một ngày tràn đầy niềm vui, luôn xinh đẹp rạng ngời nhé 🌸💖",
+  titleHeader: "💐 Chúc mừng 20/10! 💐 </br> Chúc em một ngày tràn đầy niềm vui, luôn xinh đẹp rạng ngời nhé 🌸💖",
   descriptionHeader:
     "Hôm nay là ngày đặc biệt, anh đã có một kế hoạch nho nhỏ dành riêng cho em rồi! Đi chơi với anh nhé 🎉✨",
   buttonYes: "Em đồng ý",
